d01f4a0bbcabd0274e0fbf20cf48b5dd9f9ac2bc
kuh for mac <dkrnl1318@naver.com>
2020-04-14 01:33:28 +0900

    feat:Java SimpleSelenium 자바코드 날라간거 복구중..

---
+++ spring-version/src/main/java/com/kuh/crawler/selenium/base/SeleniumCrawler.java
@@ -0,0 +1,8 @@
+package com.kuh.crawler.selenium.base;
+
+/**
+ * @author kuh
+ * @since 2020.04.13
+ */
+public class SeleniumCrawler {
+}
---
+++ spring-version/src/main/java/com/kuh/crawler/selenium/simple/SimpleSeleniumThead.java
@@ -0,0 +1,8 @@
+package com.kuh.crawler.selenium.simple;
+
+/**
+ * @author kuh
+ * @since 2020.04.14
+ */
+public class SimpleSeleniumThead {
+}
